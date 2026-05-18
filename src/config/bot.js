47ab4ca2a6533f1e07734cf59743b5395e3766d0
sigronyaliy-boop <sigronyaliy@gmail.com>
2026-05-18 20:01:27 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made by 𝕕 the real one",
+        name: "Black nigger",
         // Activity type number (0 = Playing).
         type: 2, 
       },
